5e46e5e28141e923eaa02bf64942e07820b2cacd
Sergey Korobka <nejdancs@gmail.com>
2022-07-03 22:23:03 +0300

    fix icon

--- src/js/template/pagination.js
+++ src/js/template/pagination.js
@@ -1,4 +1,5 @@
 // возвращает разметку пагинации
+import images from '../../images/sprite.svg';
 
 export function paginationListMarkup(page) {
   let item = '';
@@ -14,18 +15,22 @@ export function paginationListMarkup(page) {
       break;
     }
 
-    item += `<li class="pagination__item js-page" data-num='${i}'><a class="pagitation__link" href="#">${i}</a></li>`;
+    item += `<li class="pagination__item js-page" data-num='${i}'><a class="pagination__link" href="#">${i}</a></li>`;
   }
 
   return `
   <ul class="pagination__list">
-      <li class="button prev-js pagination__item"></li>
-      <li class="pagination__item first-js is-hidden hidden"><a class="pagitation__link" href="#">1</a></li>
-      <li class="pagination__item prevMore-js is-hidden hidden"><a class="pagitation__link" href="#">...</a></li>
+      <li class="button prev-js pagination__item"><svg class="button__icon" width="16" height ="16">
+          <use href="${images}#arrow-right"></use>
+        </svg></li>
+      <li class="pagination__item first-js is-hidden hidden"><a class="pagination__link" href="#">1</a></li>
+      <li class="pagination__item prevMore-js is-hidden hidden"><a class="pagination__link" href="#">...</a></li>
       ${item}
-      <li class="pagination__item ${isHidden} nextMore-js hidden"><a class="pagitation__link" href="#">...</a></li>
-      <li class="pagination__item ${isHidden} last-js hidden"><a class="pagitation__link" href="#">${page}</a></li>
-      <li class="button next-js pagination__item"></li>
+      <li class="pagination__item ${isHidden} nextMore-js hidden"><a class="pagination__link" href="#">...</a></li>
+      <li class="pagination__item ${isHidden} last-js hidden"><a class="pagination__link" href="#">${page}</a></li>
+      <li class="button next-js pagination__item"><svg class="button__icon" width="16" height="16">
+          <use href="${images}#arrow-left"></use>
+        </svg></li>
 
     </ul>`;
 }
