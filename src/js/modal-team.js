9e6b0bc725008c0d321a91978376d5c0d5f26aaf
Petr Mazka <petr.mazka@gmail.com>
2022-07-03 21:48:18 +0300

    FT-10
    Raw but done

--- src/js/modal-team.js
+++ src/js/modal-team.js
@@ -2,12 +2,38 @@ const teamModal = document.querySelector('.team-modal');
 const teamModalBtn = document.querySelector('.tm-close-btn');
 const teamModalOpen = document.querySelector('.footer-link');
 
-teamModalOpen.addEventListener('click', () => {
-    event.preventDefault();
-    teamModal.classList.remove('visually-hidden');
-});
+teamModalOpen.addEventListener('click', openModal);
 
-teamModalBtn.addEventListener('click', () => {
-    event.preventDefault();
-    teamModal.classList.add('visually-hidden');
-});
+function openModal(e){
+    e.preventDefault();
+    teamModal.classList.remove('visually-hidden');
+
+    window.addEventListener('keydown', closeModalByEsc);
+    window.addEventListener('click', closeModalByClick);
+    teamModalBtn.addEventListener('click', closeModal);
+    
+    function closeModalByEsc(e){
+        if(e.key === 'Escape'){
+            teamModal.classList.add('visually-hidden');
+            window.removeEventListener('keydown', closeModalByEsc);
+            window.removeEventListener('click', closeModalByClick);
+            teamModalBtn.removeEventListener('click', closeModal);
+        }
+    }
+
+    function closeModalByClick(e){
+        if(e.target === teamModal){
+            teamModal.classList.add('visually-hidden');
+            window.removeEventListener('click', closeModalByClick);
+            window.removeEventListener('keydown', closeModalByEsc);
+            teamModalBtn.removeEventListener('click', closeModal);
+        }
+    }
+
+    function closeModal(e){
+        teamModal.classList.add('visually-hidden');
+        window.removeEventListener('click', closeModalByClick);
+        window.removeEventListener('keydown', closeModalByEsc);
+        teamModalBtn.removeEventListener('click', closeModal);
+    }
+}
