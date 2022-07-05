c9595ef6b5cca98211ef0bd0de96495b66b00b0a
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-07-05 19:39:48 +0300

    commit

--- src/js/render/render-details.js
+++ src/js/render/render-details.js
@@ -1,38 +1,38 @@
-import { refs } from '../refs/refs';
-import api from '../api-service/get-deatils';
-import apiConfig from './constants/api-config';
-import { getDetails } from './api-service/get-details';
+// import { refs } from '../refs/refs';
+// import api from '../api-service/get-deatils';
+// import apiConfig from './constants/api-config';
+// import { getDetails } from './api-service/get-details';
 
-const { filmDetailsRef } = refs();
+// const { filmDetailsRef } = refs();
 
-// Получить ссылки на эл.
-// принимает data одного фильма и подставляет в соответсвующие елементы значения.
+// // Получить ссылки на эл.
+// // принимает data одного фильма и подставляет в соответсвующие елементы значения.
 
-async function renderModalDetails() {
-  const data = await getDetails('496450');
-  const imgUrl = apiConfig.IMAGE_BASE_URL;
+// async function renderModalDetails() {
+//   const data = await getDetails('496450');
+//   const imgUrl = apiConfig.IMAGE_BASE_URL;
 
-  if (!data.poster_path) {
-    filmDetailsRef.image.src = '../images/modal-img.jpg';
-    filmDetailsRef.image.alt = 'Movie photo';
-  }
+//   if (!data.poster_path) {
+//     filmDetailsRef.image.src = '../images/modal-img.jpg';
+//     filmDetailsRef.image.alt = 'Movie photo';
+//   }
 
-  filmDetailsRef.image.srcset = `${imgUrl}${data.poster_path}`;
-  filmDetailsRef.image.alt = data.title;
-  filmDetailsRef.title.textContent = data.title;
-  filmDetailsRef.voteAverage.textContent = data.vote_average;
-  filmDetailsRef.voteCount.textContent = data.vote_count;
-  filmDetailsRef.popularity.textContent = Number(data.popularity.toFixed(1));
-  filmDetailsRef.originTitle.textContent = data.original_title;
+//   filmDetailsRef.image.srcset = `${imgUrl}${data.poster_path}`;
+//   filmDetailsRef.image.alt = data.title;
+//   filmDetailsRef.title.textContent = data.title;
+//   filmDetailsRef.voteAverage.textContent = data.vote_average;
+//   filmDetailsRef.voteCount.textContent = data.vote_count;
+//   filmDetailsRef.popularity.textContent = Number(data.popularity.toFixed(1));
+//   filmDetailsRef.originTitle.textContent = data.original_title;
 
-  let genre = data.genres.map(genre => genre.name);
-  const genreList = genre.slice(0, 2);
+//   let genre = data.genres.map(genre => genre.name);
+//   const genreList = genre.slice(0, 2);
 
-  if (genre.length > 2) {
-    genreList.push('Others');
-  }
-  genre = genreList.join(', ');
+//   if (genre.length > 2) {
+//     genreList.push('Others');
+//   }
+//   genre = genreList.join(', ');
 
-  filmDetailsRef.genres.textContent = genre;
-  filmDetailsRef.about.textContent = data.overview;
-}
+//   filmDetailsRef.genres.textContent = genre;
+//   filmDetailsRef.about.textContent = data.overview;
+// }
