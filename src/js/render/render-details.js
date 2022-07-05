eebb478cf8f5abfcb311c252f105f04f5b1ca638
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-07-05 14:13:09 +0300

    commit

--- src/js/render/render-details.js
+++ src/js/render/render-details.js
@@ -5,12 +5,3 @@ const { modalRefs } = refs();
 
 // Получить ссылки на эл.
 // принимает data одного фильма и подставляет в соответсвующие елементы значения.
-
-function renderModalDetails() {
-  console.log(555555);
-  api.getDetails(496450).then(data => {
-    console.log(data);
-  });
-  //   console.log(movieInfo);
-  // const imgSrc =
-}
