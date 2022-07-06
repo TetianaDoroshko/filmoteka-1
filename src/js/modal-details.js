3b5578e0034db8d9a4db25012b15e77f23433f3c
MariaK1506 <93434705+MariaK1506@users.noreply.github.com>
2022-07-06 23:39:32 +0300

    add modal functional

--- src/js/modal-details.js
+++ src/js/modal-details.js
@@ -4,43 +4,24 @@
 // нужно при открытии вызывать функцию реализованую в файлах queue или watched которая проверяет
 // есть ли фильм в хранилище и подсвечивает кнопки.
 
-import { refs } from './refs/refs';
-import apiConfig from './constants/api-config';
+// import { refs } from './refs/refs';
 import { getDetails } from './api-service/get-details';
+import { onOpenModal } from './open-close-modal-details';
+import { renderModalDetails } from './render/render-details';
 
-const { filmDetailsRef } = refs();
+// const { filmDetailsRef } = refs();
 
-// const btn = document.querySelector('.modal-btn');
+export async function onFilmClick(event) {
+  event.preventDefault();
+  // console.log(event.target);
 
-// btn.addEventListener('click', renderModalDetails);
+  if (!event.target.classList.contains('gallery-card')) {
+    return;
+  } /* стиль лишки, на нем и id */
 
-export async function renderModalDetails() {
-  const data = await getDetails('496450');
-  const imgUrl = apiConfig.IMAGE_BASE_URL;
-
-  if (!data.poster_path) {
-    filmDetailsRef.image.src = '../images/modal-img.jpg';
-    filmDetailsRef.image.alt = 'Movie photo';
-  }
-
-  filmDetailsRef.image.src = `${imgUrl}${data.poster_path}`;
-  filmDetailsRef.image.alt = data.title;
-  filmDetailsRef.title.textContent = data.title;
-  filmDetailsRef.voteAverage.textContent = data.vote_average;
-  filmDetailsRef.voteCount.textContent = data.vote_count;
-  filmDetailsRef.popularity.textContent = Number(data.popularity.toFixed(1));
-  filmDetailsRef.originTitle.textContent = data.original_title;
-
-  // let genre = data.genres.map(genre => genre.name);
-  // const genreList = genre.slice(0, 2);
-
-  // if (genre.length > 2) {
-  //   genreList.push('Others');
-  // }
-  // genre = genreList.join(', ');
-
-  filmDetailsRef.genres.textContent = data.genres
-    .map(genre => genre.name)
-    .join(', ');
-  filmDetailsRef.about.textContent = data.overview;
+  let movieId = event.target.dataset.id;
+  getDetails(movieId);
+  // getDetails('496450');
+  await renderModalDetails(movieId);
+  onOpenModal();
 }
--- src/js/open-close-modal-details.js
+++ src/js/open-close-modal-details.js
@@ -2,46 +2,27 @@
 // должна закрывать по esc и по клику в бекдроп
 
 import { refs } from './refs/refs';
-import { getDetails } from './api-service/get-details';
-import { renderModalDetails } from './modal-details';
 import { modalButtonsHandler } from './modal-details/watched';
-
+import { onFilmClick } from './modal-details';
 const { filmDetailsRef } = refs();
 
-const btn = document.querySelector('.js-btn-queue'); /* временная кнопка */
+/* временная кнопка */
 
-btn.addEventListener('click', onFilmClick); /*  */
+// const btn = document.querySelector('.js-btn-queue');
+// btn.addEventListener('click', onFilmClick); /*  */
+
 filmDetailsRef.filmDetailsModalClose.addEventListener('click', onCloseModal);
 filmDetailsRef.modalBackdrop.addEventListener('click', onBackdropClick);
+filmDetailsRef.filmDetailsModalContainer.addEventListener('click', onFilmClick);
 
-// filmDetailsRef.filmDetailsModalContainer.addEventListener(
-//   'click',
-//   onFilmClick
-// ); /* нужно потом навесить слушателя на контейнер с карточками */
-
-async function onFilmClick(event) {
-  event.preventDefault();
-  //   console.log(event.target);
-
-  //   if (!event.target.classList.contains('gallery-card')) {
-  //     return;
-  //   } стиль лишки, на нем и id
-
-  //   let movieId = event.target.dataset.id;
-  //   getDetails(movieId);
-  getDetails('496450');
-  await renderModalDetails();
-  onOpenModal();
-}
-
-function onOpenModal() {
+export function onOpenModal() {
   window.addEventListener('keydown', onCloseModalByEscape);
   document.body.classList.add('show-modal');
   filmDetailsRef.filmDetailsModal.classList.remove('is-hidden');
   modalButtonsHandler(); /* Это Танина функция - запуск кнопок при открытии модалки */
 }
 
-function onCloseModal() {
+export function onCloseModal() {
   window.removeEventListener('keydown', onCloseModalByEscape);
   document.body.classList.remove('show-modal');
   filmDetailsRef.filmDetailsModal.classList.add('is-hidden');
