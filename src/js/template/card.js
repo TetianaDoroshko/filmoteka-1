c15950d3b138af60254340514e784cb2dd3db103
Sergey Korobka <nejdancs@gmail.com>
2022-07-07 00:11:06 +0300

    fix markup

--- src/js/template/card.js
+++ src/js/template/card.js
@@ -7,12 +7,14 @@ export function createSingleMovieMarkup(movie, listOfGenres, movieYear) {
   const movieName = movie.name || movie.original_title;
 
   return `<li class="gallery-card" data-id ="${movie.id}">
-<a class="gallery-card__item">
+<a class="gallery-card__link">
             <img src="${IMAGE_BASE_URL + movie.poster_path}" alt="${
     movie.original_title
   }"  class="gallery-card__image" >
+  <div class="gallery-card__info">
 <p class="gallery-card__name">${movieName}</p>
-<p class="gallery-card__genre">${listOfGenres} | ${movieYear}<span class="gallery-card__span"></span></p>
-</a>        
+<p class="gallery-card__genre">${listOfGenres} | ${movieYear}</p>
+</div>
+</a>
 	    </li>`;
 }
